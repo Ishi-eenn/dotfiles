822db55323f2b23f6bfc8bbc65922fee3d346c34
Ishi_eenn <syi378039@gmail.com>
2026-02-14 16:17:47 +0900

    whichkeyを透明化

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,8 +1,12 @@
--- 行番号の色を固定（colorscheme/TransparentEnable後に毎回当てる）
 vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
   callback = function()
-    -- 背景は透明のまま、文字色だけ変える
     vim.api.nvim_set_hl(0, "LineNr", { fg = "#a0a9cb", bg = "NONE" })
     vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#e0af68", bg = "NONE", bold = true })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "WhichKeyFloat",  { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "WhichKeyBorder", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "WhichKeyTitle",  { bg = "NONE" })
   end,
 })
---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/which-key.nvim",
+  opts = function(_, opts)
+    opts.win = opts.win or {}
+    opts.win.wo = opts.win.wo or {}
+    opts.win.wo.winblend = 0
+    opts.win.wo.winhighlight =
+      "Normal:WhichKeyNormal,NormalFloat:WhichKeyNormal,FloatBorder:WhichKeyBorder"
+  end,
+}
