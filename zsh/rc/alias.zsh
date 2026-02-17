c174d2dde85bfd0401d7d0322d9499f34cd6fda9
Ishi_eenn <syi378039@gmail.com>
2026-02-17 22:34:17 +0900

    aliasを追加

--- zsh/rc/alias.zsh
+++ zsh/rc/alias.zsh
@@ -5,6 +5,11 @@ alias lcaer='clear'
 # AtCoder提出コマンド
 alias ojt='function _ojt() { g++ -std=c++23 "$1/main.cpp" && oj t -c "./a.out" -d "$1/sample"; }; _ojt'
 alias oja='function _answer() { cd "$1" && acc s && cd ..; }; _answer'
+pbc() {
+  local dir="${1:?usage: pbc <dir> [file]}"
+  local file="${2:-main.cpp}"
+  cat "$dir/$file" | pbcopy
+}
 
 # cd-bookmark
 alias cdb='cd-bookmark'
