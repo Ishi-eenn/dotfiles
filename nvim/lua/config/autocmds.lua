488b4759241c46ffd848e6c00e694e51685b7272
Ishi_eenn <syi378039@gmail.com>
2026-02-20 22:18:43 +0900

    保存時に行末のスペースを削除

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -14,3 +14,22 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    -- Markdown は行末スペースに意味があることがあるので、不要ならこの行は消してOK
+    if vim.bo.filetype == "markdown" then
+      return
+    end
+
+    -- カーソル位置・検索レジスタを維持してトリム
+    local view = vim.fn.winsaveview()
+    local search = vim.fn.getreg("/")
+
+    vim.cmd([[silent! %s/\s\+$//e]])
+
+    vim.fn.setreg("/", search)
+    vim.fn.winrestview(view)
+  end,
+})
